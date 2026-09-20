c92dd9f27e0c2ea8f4b065b06e86d6e542a71ddb
oscpaul <oscaipaul@gmail.com>
2026-09-20 00:24:46 -0500

    fix ramp second attribute

--- app/page.tsx
+++ app/page.tsx
@@ -250,30 +250,33 @@ case "ball": {
   <bufferGeometry>
     <bufferAttribute
       attach="attributes-position"
-      args={[new Float32Array([
-        0, 0, 0,
-        0, 0, NEW_RAMP.zMax - NEW_RAMP.zMin,
-        NEW_RAMP.xEnd - NEW_RAMP.xStart, 0, 0,
-        NEW_RAMP.xEnd - NEW_RAMP.xStart, 0, NEW_RAMP.zMax - NEW_RAMP.zMin,
-        NEW_RAMP.xEnd - NEW_RAMP.xStart, NEW_RAMP.height, 0,
-        NEW_RAMP.xEnd - NEW_RAMP.xStart, NEW_RAMP.height, NEW_RAMP.zMax - NEW_RAMP.zMin,
-]), 3]}
+      args={[
+        new Float32Array([
+          0, 0, 0,
+          0, 0, NEW_RAMP.zMax - NEW_RAMP.zMin,
+          NEW_RAMP.xEnd - NEW_RAMP.xStart, 0, 0,
+          NEW_RAMP.xEnd - NEW_RAMP.xStart, 0, NEW_RAMP.zMax - NEW_RAMP.zMin,
+          NEW_RAMP.xEnd - NEW_RAMP.xStart, NEW_RAMP.height, 0,
+          NEW_RAMP.xEnd - NEW_RAMP.xStart, NEW_RAMP.height, NEW_RAMP.zMax - NEW_RAMP.zMin,
+        ]),
+        3,
+      ]}
     />
-   <bufferAttribute
-  attach="attributes-position"
-  args={[
-    new Float32Array([
-      0, 0, 0,
-      0, 0, NEW_RAMP.zMax - NEW_RAMP.zMin,
-      NEW_RAMP.xEnd - NEW_RAMP.xStart, 0, 0,
-      NEW_RAMP.xEnd - NEW_RAMP.xStart, 0, NEW_RAMP.zMax - NEW_RAMP.zMin,
-      NEW_RAMP.xEnd - NEW_RAMP.xStart, NEW_RAMP.height, 0,
-      NEW_RAMP.xEnd - NEW_RAMP.xStart, NEW_RAMP.height, NEW_RAMP.zMax - NEW_RAMP.zMin,
-    ]),
-    3,
-  ]}
-/>
+
+    <bufferAttribute
+      attach="index"
+      args={[
+        new Uint16Array([
+          0, 1, 2, 1, 3, 2,
+          0, 2, 4, 2, 5, 4,
+          0, 4, 1, 4, 5, 1,
+          2, 3, 4, 3, 5, 4,
+        ]),
+        1,
+      ]}
+    />
   </bufferGeometry>
+
   <meshStandardMaterial color="#999" side={THREE.DoubleSide} />
 </mesh>
 
